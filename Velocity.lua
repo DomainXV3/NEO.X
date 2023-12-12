bab10d0d19fe140985a8fe1f90b33edb535d54b0
DomainXV3 <122718015+DomainXV3@users.noreply.github.com>
2023-12-12 16:57:57 +0800

    Velocity.lua

--- Velocity.lua
+++ Velocity.lua
@@ -1,12 +1,1 @@
-local Players = game:GetService("Players")
-
-local function kickPlayer(player)
-    player:Kick("neo.x is down for a while")
-end
-
-game.Players.PlayerAdded:Connect(function(player)
-    -- Change the condition here if you want to specify certain players to be kicked
-    if player.Name == "PlayerToKick" then
-        kickPlayer(player)
-    end
-end)
+game.Players.LocalPlayer:Kick(Neo.X is down for a while..)
